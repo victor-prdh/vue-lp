2c9484a471541cc958fd7de71ac876f853ce6eec
Victor Prudhomme <contact@victor-prdh.com>
2023-03-01 15:58:37 +0100

    clean

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,5 @@ const app = createApp(App);
 app.use(router);
 
 app.mount("#app");
+
+// URL = https://api.themoviedb.org/3/discover/movie?api_key=00f991e8dde20b13bd9e515bb20c6fdd&sort_by=popularity.desc&language=fr
